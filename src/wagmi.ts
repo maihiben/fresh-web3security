28b4421f2ab837c82186a5147030d1bd2629779b
maihiben <maihiben@gmail.com>
2025-07-17 23:28:54 +0100

    Update wagmi.ts

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -7,12 +7,11 @@ import {
   bsc,
   avalanche,
   fantom,
-  sepolia,
 } from 'wagmi/chains';
 
 export const config = getDefaultConfig({
   appName: 'Web3Security',
   projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!,
-  chains: [mainnet, polygon, optimism, arbitrum, bsc, avalanche, fantom, sepolia],
+  chains: [mainnet, polygon, optimism, arbitrum, bsc, avalanche, fantom],
   ssr: true,
 }); 
